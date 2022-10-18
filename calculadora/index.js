04a8767006716aa2654dafc9b9b4b23829e01aa8
Isadora <106554588+IsadoraAlcantara@users.noreply.github.com>
2022-10-18 13:24:33 -0300

    Add files via upload

--- calculadora/index.js
+++ calculadora/index.js
@@ -1,19 +1,26 @@
-let visor = document.getElementById('visor');
-
+// Variaveis de classe 
+let btnNumeros = document.getElementsByClassName('btnNum');
+let btnOperadores = document.getElementsByClassName('btnOpera');
+// Variaveis de id
+let botaoC = document.getElementById('btnApaga');
+let botaoIgual = document.getElementById('btnIgual')
+let visor = document.getElementById('tela');
+// Valores 
 let valor_salvo = null;
 let operador_salvo = null;
 let resultado = null;
-
-let btnNumeros = document.getElementsByClassName('botao-numero');
-
-for (let botao of btnNumeros) {
-    botao.addEventListener('click', clica_numero);
-}
-
+// Adiçao de eventos 
+botaoIgual.addEventListener('click', calcula_resultado);
+botaoC.addEventListener("click", limpa_visor);
+// laço para cada clique nos btn
+for (let botao of btnNumeros)botao.addEventListener('click', clica_numero); 
+// Função de clique 
 function clica_numero(event){
-    (isNaN(visor.innerHTML) === true) ? visor.innerHTML = event.target.innerHTML : visor.innerHTML = visor.innerHTML + event.target.innerHTML;
-    }
-
+(isNaN(visor.innerHTML) === true) ? visor.innerHTML = event.target.innerHTML : visor.innerHTML = visor.innerHTML + event.target.innerHTML;
+}
+// Laço para clique em botoes de operaçao
+for (let botao of btnOperadores) botao.addEventListener("click", clica_operador);
+// Funçao de clique em operaçao
 function clica_operador(event){
     if (isNaN(visor.innerHTML) === false){
         if (valor_salvo === null){
@@ -26,37 +33,13 @@ function clica_operador(event){
     visor.innerHTML = event.target.innerHTML;
     operador_salvo = event.target.innerHTML;
 }
-
-let btnOperadores = document.getElementsByClassName('botao-operador');
-for (botao of btnOperadores) {
-    botao.addEventListener('click', clica_operador);
-
-}
-
-// botão C
-let C = document.getElementsByClassName('C')[0];
-C.addEventListener('click', limpa_visor);
-function limpa_visor(event) {
-    visor.innerHTML = "";
-}
-// botão resultado
-let botaoResultado = document.getElementById('btnResultado')
-botaoResultado.addEventListener('click', calcula_resultado);
-function calcula_resultado(event) {
-    if(valor_salvo != null && operador_salvo != null && isNaN(visor.innerHTML) === false){
-        let resultado = executa_operacao(valor_salvo, operador_salvo, Number(visor.innerHTML));
-        visor.innerHTML = resultado;
-        valor_salvo = null;
-        operador_salvo = null;
-    }
-}
-
+// Funçao de limpar os numeros e resultados
 function limpa_visor(event){
     visor.innerHTML = "";
     valor_salvo = null;
     operador_salvo = null;
 }
-
+// Funçao para calcular o resultado
 function calcula_resultado(event) {
     if(valor_salvo != null && operador_salvo != null && isNaN(visor.innerHTML) === false){
         let resultado = executa_operacao(valor_salvo, operador_salvo, Number(visor.innerHTML));
@@ -65,22 +48,14 @@ function calcula_resultado(event) {
         operador_salvo = null;
     }
 }
-
-let body = document.getElementById(`body`);
-body.addEventListener('keypress', pressionou_tecla);
-function pressionou_tecla(event) {
-    if (event.key === "+") {
-        visor.innerHTML = `+`;
-    }
-}
-
+// Funçao para executar as operaçoes 
 function executa_operacao(valor1, operador, valor2){
     if (operador === "+") return valor1 + valor2;
     else if (operador === "-") return valor1 - valor2;
     else if (operador === "*") return valor1 * valor2;
     else if (operador === "/") return valor1 / valor2
     }
-
+// Funçao para o teclado funcionar 
 document.getElementsByTagName('body')[0].addEventListener('keydown', pressiona_tecla);
 function pressiona_tecla(event) {
      (event.key === '*') ? document.getElementById('multiplicacao').click() :
@@ -97,5 +72,6 @@ function pressiona_tecla(event) {
      (event.key === '+' )? document.getElementById('adicao').click() :
      (event.key === '/') ? document.getElementById('divisao').click() :
      (event.key === '.') ? document.getElementById('ponto').click() :
-     (event.keyCode === '=') ? botaoResultado.click() : botaoC.click();
+     (event.keyCode === '=') ? botaoIgual.click() : botaoC.click();
 }
+
